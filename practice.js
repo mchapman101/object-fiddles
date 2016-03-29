8a1bef6f0cc067e191bbe2f44bc6483d0b7946d9
MarkChapman <mchapman101@yahoo.com>
2016-03-29 14:54:49 -0600

    thigs

--- practice.js
+++ practice.js
@@ -5,6 +5,11 @@
 
   //Code here
 
+var me = {
+  name: 'Mark',
+  age: '32',
+};
+
 
 
 
@@ -17,16 +22,28 @@
 
   //Code here
 
+var favoriteThings = {
+  band: "Daft Punk",
+  food: "Steak",
+  person: 'Wife',
+  book: "Yeah",
+  movie: "Lord of the Rings",
+  holiday: "Christmas",
+}
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Code here
 
+  favoriteThings.car = 'Ford';
+  favoriteThings.brand = 'Apple';
+
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
   //Code here
-
+favoriteThings.food = 'Lettuce';
+favoriteThings.book = '50 Shades of Gray';
 
 
 
@@ -44,15 +61,19 @@ that is named color, with the value being the color of your backpack. */
 
   //Code here
 
+var backPack = {};
+var item = 'firstPocket';
+backPack[item] = 'chapstick';
+
 //After you do the above, alert your entire backPack object.
 
-  //Code here
+// alert(backPack);  //Code here
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
-  //Code here
+console.log(backPack);  //Code here
 
 
 
@@ -64,50 +85,67 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 //Create an 'alsoMe' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
 
-  //Code Here
+var alsoMe = {
+  name: "Mark",
+  age: "32",
+  height: "6ft3in",
+  gender: 'Male',
+  married: "yes",
+  eyeColor: "hazel",
+  hairColor: "brown",
+}
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
   //Code Here
-
-
-
-
+//
+// for(key in alsoMe) {
+//   alert(alsoMe[key]);
+// }
 //NEXT PROBLEM
-
-
-
 
 //Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
 
-  //Code Here
+var album = {
+  greenPeas: "2m3s",
+  iLovePeas: "1m34s",
+  goForPeas: "4m56s",
+  peasForPresident: "12m2s",
+  peasPeasPeas: "1m1s",
+}  //Code Here
 
 //Now, loop through your album object alerting every song title individually.
 
   //Code Here
-
-
-
-
+//
+// for(key in album){
+//   alert(key);
+// }
+// //
 //NEXT PROBLEM
-
-
-
-
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
-  //Code Here
+var states = {
+  CA: 12000000,
+  UT: 800000,
+  CO: 140000,
+  NV: 3000,
+  AZ: 420000,
+
+}  //Code Here
 
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
   //Code Here
-
-
+//
+// for(key in states) {
+//   if(states[key] > 30000) {
+//     alert(key);
+//   };
+// }
 
 
 //NEXT PROBLEM
-
-
 
 
 var user1 = {
@@ -117,22 +155,26 @@ var user1 = {
     birthday: undefined,
     username: 'tylermcginnis33',
     age: 0
-}
+};
 /*Above you're given a user object. Loop through the user object checking to make sure
 that each value is truthy. If it's not truthy, remove it from the object. */
 
-  //Code Here
+  for(key in user1) {
+    if(!user1[key]) {
+      delete user1[key];
+    }
+  }
+
 
 //Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Code Here
-
-
-
+var user1 = {
+  name: "Mark",
+  username: "blahblah",
+  pwHash: 'Wjsihf!3jhfs'
+};
 
 //NEXT PROBLEM
-
-
 
 
 var user2 = {
@@ -149,33 +191,36 @@ var user2 = {
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
+user2.name = "Tyler S. McGinnis";
+user2.email = "tyler.mcginnis@devmounta.in";
   //Code Here
 
 //Now call the sayName method that's on the user object which will alert the users email
 
   //Code Here
-
-
-
+// user2.sayName();
 
 //NEXT PROBLEM
 
-
-
-
 //Create an empty object called methodCollection.
 
-  //Code Here
+var methodCollection = {}  //Code Here
 
 /*Now add two methods (functions that are properties on objects) to your methodCollection
 object. One called 'alertHello' which alerts 'hello' and another method called logHello
  which logs 'hello' to the console. */
 
-  //Code Here
+methodCollection.alertHello = function(){
+  alert("hello");
+  }//Code Here
 
+methodCollection.logHello = function(){
+  console.log("hello");
+}//Code Here
 //Now call your alertHello and logHello methods.
 
-  //Code Here
+// methodCollection.alertHello();
+// methodCollection.logHello();
 
 
 
@@ -186,6 +231,14 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 // Create a function called MakePerson which takes in name, birthday, ssn as its parameters and returns a new object with all of the information that you passed in.
 
   //Code Here
+
+function MakePerson(name, birthday, ssn) {
+  this.name = name;
+  this.birthday = birthday;
+  this.snn = ssn;
+};
+
+// return MakePerson("mark", "sept", 123121234);
 
 
 
